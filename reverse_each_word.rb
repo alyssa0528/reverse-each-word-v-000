eccc176f5040b2363c98c31e54020378f43b443e
Alyssa Kim <alyssa.kim@gmail.com>
2018-02-06 13:11:51 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,10 @@ def reverse_each_word(sentence)
   end 
   reversed_sentence.join(" ")
 end
+
+def reverse_each_word(sentence)
+  sentence_array = sentence.split(" ")
+  sentence_array.collect do |word|
+    word.reverse
+  end 
+end 
