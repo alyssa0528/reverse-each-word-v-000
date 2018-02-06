2ee8376edde67a9e123be9cedebf2c8a88bb9d21
Alyssa Kim <alyssa.kim@gmail.com>
2018-02-06 12:57:30 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(sentence)
-  sentence_array = sentence.split(" ")
+  reversed_sentence = []
+  sentence_array = sentence.split(" ") # [hello, there, and, how, are, you?]
   sentence_array.each do |word|
-    word.reverse 
+  reversed_sentence << word.reverse 
   end 
 end
